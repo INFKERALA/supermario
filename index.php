ddee9a72e53b43cf97e89a782409104543807c44
Vyshnav NK <60059196+vizzhacker@users.noreply.github.com>
2020-01-31 18:35:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-<?php include_once("index.html"); ?>
+<?php 
+<html>
+ <head>
+        <meta http-equiv="refresh" content="3;url=index.html" />
+    </head>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bgimg {
+  background: black;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+<body>
+
+<div class="bgimg">
+  <div class="topleft">
+    <p>InfoSecians</p>
+  </div>
+  <div class="middle">
+    <h1>COMING SOON</h1>
+    <hr>
+    <p>Till that play this!!!</p>
+  </div>
+
+</div>
+
+</body>
+</html>
+ ?>
